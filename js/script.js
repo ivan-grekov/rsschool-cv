eece8355b6a72c19c312d0b4f30a7bea18961aaa
Ivan Grekov <ivan_grekov@yahoo.com>
2022-03-13 15:10:50 +0300

    feat: add slider component for section 'project'

---
+++ js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// !Slider component
+
+const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+  slides.forEach(function (s, i) {
+    s.style.transform = `translateX(${100 * (i - slide)}%)`;
+  });
+};
+
+goToSlide(0);
+
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+btnRight.addEventListener("click", nextSlide);
+
+// Previous slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+btnLeft.addEventListener("click", prevSlide);
+
+// Add keyboard events
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    prevSlide();
+  } else if (e.key === "ArrowRight") {
+    nextSlide();
+  }
+});
