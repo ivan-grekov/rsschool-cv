df0dcfeb4da73662dfc846655fe11dc56bc24023
Ivan Grekov <ivan_grekov@yahoo.com>
2022-09-10 22:05:11 +0300

    feat: add smooth scrolling for page

--- script.js
+++ script.js
@@ -92,3 +92,35 @@ const headerEl = document.querySelector(".header");
 buttonActiveNav.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// !SMOOTH SCROLLING ANIMATION
+
+const allLinks = document.querySelectorAll(".main-nav a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    // Close mobile navigation
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
